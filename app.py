4761ffc223db66d6b69d397bbcb505db4d32e359
Rahul Roy <29518806+rahulroy0611@users.noreply.github.com>
2025-03-18 07:04:51 +0000

    Updated

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.text("Hello GeeksForGeeks!!!")
+st.text("Welcome Bug Hunters !!!")
+
+domain = st.text_input("Enter the domain name")
+
+if (st.button("Submit")):
+  result = domain.title()
+  st.success(result)
