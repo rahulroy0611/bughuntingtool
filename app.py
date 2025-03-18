7a8618b51333fad6bf2f348b33ca76f14d80d401
Rahul Roy <29518806+rahulroy0611@users.noreply.github.com>
2025-03-18 09:23:18 +0000

    check

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import streamlit as st
+import re
 
 st.title("Welcome Bug Hunters Group1!!!")
 
 domain = st.text_input("Enter the domain name")
 
-if (st.button("Submit")):
-  result = domain.title()
-  st.success(result)
+
+def is_valid_domain(domain):
+  pattern = r'^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'
+  if re.match(pattern, domain):
+    return True
+  else:
+    return False
+
+
+if is_valid_domain(domain):
+  st.success("The domain name is valid.")
+else:
+  st.error("The domain name is invalid.")
