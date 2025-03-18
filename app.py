8feddb628db743a8b2a0a2d0061e78998c5daf39
Rahul Roy <29518806+rahulroy0611@users.noreply.github.com>
2025-03-18 10:32:58 +0000

    new change

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
-
 import streamlit as st
 import re, requests
-import os
-
-if __name__ == "__main__":
-    st.set_page_config(page_title="Bug Hunters Group")
 
 st.title("Welcome Bug Hunters Group!!!")
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-channel_id = "2580630440"
+bot_token = "7970524018:AAHYtReX0EtEU1Wjg7rWU8s5Y5z6BkJ9h18"
+channel_id = "-1002580630440"
+url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage'
+
 
 def send_message(message):
-    if not bot_token:
-        st.error("Telegram bot token not found. Please set it in Secrets.")
-        return
-        
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    params = {
-        'chat_id': channel_id,
-        'text': message
-    }
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        st.success("Message sent successfully!")
-    except requests.exceptions.RequestException as e:
-        st.error(f"Failed to send message: {str(e)}")
+  params = {
+      'chat_id':
+      channel_id,  # For username use '@channel_name', for ID use channel ID
+      'text': message
+  }
+  response = requests.get(url, params=params)
+  print(response.json())
+  if response.status_code == 200:
+    st.success("Message sent successfully!")
+  else:
+    st.error("Failed to send message.")
+
 
 def is_valid_domain(domain):
-    pattern = r'^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'
-    if re.match(pattern, domain):
-        return True
-    else:
-        return False
+  pattern = r'^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'
+  if re.match(pattern, domain):
+    return True
+  else:
+    return False
+
 
 def subdomain(domain):
-    message = f"New domain submitted for checking: {domain}"
-    send_message(message)
+  send_message(f"Subdomain {domain} submitted for testing")
+
 
 domain = st.text_input("Enter the domain name")
 action = st.multiselect("Action: ",
-                       ["Subdomain Enumeration", "Subdomain Takeover"])
+                        ["Subdomain Enumeration", "Subdomain Takeover"])
 
 if (st.button("Check")):
-    if is_valid_domain(domain):
-        subdomain(domain)
-    else:
-        st.error("The domain name is invalid.")
+  if is_valid_domain(domain):
+    subdomain(domain)
+  else:
+    st.error("The domain name is invalid.")
